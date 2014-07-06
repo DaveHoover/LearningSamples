3479366353ddc8d950054e7b37fd4008b85c5ba3
DaveHoover <dmh_pub@live.com>
2014-07-05 19:34:37 -0700

    Update FileListCollection.cs

--- MediaFileSorter/MediaFileSorter/Model/FileListCollection.cs
+++ MediaFileSorter/MediaFileSorter/Model/FileListCollection.cs
@@ -6,7 +6,19 @@ using System.Collections.ObjectModel;
 
 namespace MediaFileSorter.Model
 {
+      
     public class FileListCollection :ObservableCollection<string>
     {
+        
+        /// <summary>
+        /// add something to see what happens in GitHub
+        /// 1st submission did not fully work 2nd attempt
+        /// 3rd attempt 
+        /// 4rth attempt
+        /// 5th attemp
+        /// </summary>
+        public FileListCollection()
+        {
+        }        
     }
 }
